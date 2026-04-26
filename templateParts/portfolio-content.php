ecc8afeed206220e8a626d45ba9b3eb486964160
Themis <you@example.com>
2026-04-26 21:56:01 +0200

    26/4 -6

--- templateParts/portfolio-content.php
+++ templateParts/portfolio-content.php
@@ -54,11 +54,11 @@
   <h2 class="py-4 text-center border-bottom mb-4" id="portfolio">Portfolio</h2>
 
   <div class="row d-flex justify-content-center category-menu text-center mb-5">
-      <button class="col-5 my-2 mx-2 py-1 col-md-3 col-lg-2 border-0 shadow text-center rounded  filter-btn active-filter" data-category="All">All</button>
+      <button class="col-5 my-2 bg-white mx-2 py-1 col-md-3 col-lg-2 border-0 shadow text-center rounded  filter-btn active-filter" data-category="All">All</button>
       <?php 
       $terms = get_terms(['taxonomy' => 'portfolio_type', 'hide_empty' => true]);
       foreach($terms as $term) {
-          echo '<button class="col-6 my-2 mx-2 py-1 col-md-4 col-lg-3 border-0 shadow text-center rounded filter-btn" data-category="' . $term->slug . '">' . $term->name . '</button>';
+          echo '<button class="col-6 bg-white my-2 mx-2 py-1 col-md-4 col-lg-3 border-0 shadow text-center rounded filter-btn" data-category="' . $term->slug . '">' . $term->name . '</button>';
       }
       ?>
   </div>
